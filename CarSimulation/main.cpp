6e65c6c744e4c71dc6de5e21c624a0aa3f0717e4
cameronbowen444 <cameronbowen555@gmail.com>
2024-12-18 10:34:16 -0800

    Initial Commit

---
+++ CarSimulation/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CarSimulation
+//
+//  Created by Cameron Bowen on 12/18/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
